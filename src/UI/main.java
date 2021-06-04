8596e433f958bd8e357532e06c4b0354ade97d9b
hieupro9c0 <51318006+hieupro9c0@users.noreply.github.com>
2021-06-04 08:28:41 +0700

    Update main.java

--- src/UI/main.java
+++ src/UI/main.java
@@ -8,13 +8,16 @@ public class main {
 		Validation v = new Validation();
 		DepartmentManage d = new DepartmentManage();
 		while(true) {
-		int input = v.getValidInt(1, 2);
-		switch(input) {
+			System.out.println("1: Add Employee");
+			System.out.println("2. Display Employee");
+			System.out.println("3: Enter your choice:");
+			int input = v.getValidInt(1, 2);
+			switch(input) {
 		case 1:
 			d.addEmployee();
 			break;
 		case 2:
-			d.listDepartment();
+			d.displayEmployee();
 			break;
 		}
 		
